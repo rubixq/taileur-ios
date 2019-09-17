b0090eff41df853e5f1a3dfe55ffd035a67ea6e6
yaw koranteng boafo <koranteng@smsgh.com>
2019-09-17 16:02:16 +0000

    solved this issue

--- Taileur/UserInterface/Components/Explore/HomeComponent/HomeViewController.swift
+++ Taileur/UserInterface/Components/Explore/HomeComponent/HomeViewController.swift
@@ -11,34 +11,28 @@ import UIKit
 class HomeViewController: UIViewController {
 	let searchController = UISearchController(searchResultsController: nil)
 	
+	var data : [ProductSummary] = [ProductSummary(image: #imageLiteral(resourceName: "kafdab"), name: "Shirt and Trouser", category: "Dress", price: "GHS109"),ProductSummary(image: #imageLiteral(resourceName: "kafdab"), name: "Shirt", category: "Dress", price: "GHS109"),ProductSummary(image: #imageLiteral(resourceName: "kafdab"), name: "Shirt", category: "Dress", price: "GHS109"),ProductSummary(image: #imageLiteral(resourceName: "kafdab"), name: "Shirt", category: "Dress", price: "GHS109"),ProductSummary(image: #imageLiteral(resourceName: "kafdab"), name: "Shirt", category: "Dress", price: "GHS109"),ProductSummary(image: #imageLiteral(resourceName: "kafdab"), name: "Shirt", category: "Dress", price: "GHS109"),ProductSummary(image: #imageLiteral(resourceName: "kafdab"), name: "Shirt", category: "Dress", price: "GHS109"),ProductSummary(image: #imageLiteral(resourceName: "kafdab"), name: "Shirt", category: "Dress", price: "GHS109")]
+	
 	lazy var homeCollectionView: UICollectionView = {
 		let collection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
 		collection.translatesAutoresizingMaskIntoConstraints = false
 		collection.delegate  = self
 		collection.dataSource = self
+		collection.register(CollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "CollectionViewHeader")
 		collection.register(TopCell.self, forCellWithReuseIdentifier: "TopCell")
 		collection.register(MiddleCell.self, forCellWithReuseIdentifier: "MiddleCell")
-		collection.register(BottomCell.self, forCellWithReuseIdentifier: "BottomCell")
-
+		collection.register(BottomCellCollectionViewCell.self, forCellWithReuseIdentifier: "BottomCellCollectionViewCell")
+    collection.showsVerticalScrollIndicator = false
+		collection.showsHorizontalScrollIndicator = false
 		collection.backgroundColor = .white
 		return collection
 	}()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-			self.navigationController?.navigationBar.prefersLargeTitles = true
-			self.title = "Home"
-		//	self.navigationItem.searchController = searchController
-			//searchController.searchBar.placeholder = "Search product"
-			//searchController.searchBar.searchBarStyle = .minimal
-			//searchController.searchBar.delegate = self
-			//searchController.searchBar.backgroundColor = .red
-	  	//searchController.searchBar.textField?.backgroundColor = .yellow
-			//searchController.searchBar.textField?.tintColor = .yellow
-		//	definesPresentationContext = true
-		//	searchController.obscuresBackgroundDuringPresentation = false
-    }
+	override func viewDidLoad() {
+			super.viewDidLoad()
+		self.navigationController?.navigationBar.prefersLargeTitles = true
+		self.title = "Home"
+	}
 	override func viewDidAppear(_ animated: Bool) {
 		homeCollectionView.reloadData()
 	}
@@ -51,7 +45,6 @@ class HomeViewController: UIViewController {
 }
 
 extension HomeViewController : UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
-	
 	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 		switch indexPath.section {
 		case 0:
@@ -59,12 +52,13 @@ extension HomeViewController : UICollectionViewDelegate,UICollectionViewDataSour
 		case 1:
 			return CGSize(width: collectionView.frame.width, height: 105)
 		case 2:
-			return CGSize(width: collectionView.frame.width, height:  collectionView.frame.height)
+			let lay = collectionViewLayout as! UICollectionViewFlowLayout
+        let width =  (collectionView.frame.width / 2) - lay.minimumInteritemSpacing
+			return CGSize(width: width, height:  280)
 		default:
 			return CGSize(width: collectionView.frame.width, height: 100)
 		}
 	}
-	
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 		switch section {
 		case 0:
@@ -72,12 +66,12 @@ extension HomeViewController : UICollectionViewDelegate,UICollectionViewDataSour
 		case 1 :
 			return 1
 		case 2 :
-			return 1
+			
+			return data.count
 		default:
 			return 1
 		}
 	}
-	
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 		switch indexPath.section {
 		case 0:
@@ -90,32 +84,112 @@ extension HomeViewController : UICollectionViewDelegate,UICollectionViewDataSour
 			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MiddleCell", for: indexPath) as! MiddleCell
 			return cell
 		case 2:
-			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BottomCell", for: indexPath) as! BottomCell
+			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BottomCellCollectionViewCell", for: indexPath) as! BottomCellCollectionViewCell
+			let product = data[indexPath.item]
+			cell.itemImage.image = product.image
+			cell.itemName.text = product.name
+			cell.categoryLabel.text = product.category
+			cell.itemPrice.text = product.price
 			return cell
 		default:
 			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TopCell", for: indexPath) as! TopCell
 			return cell
 		}
 	}
-	
 	func numberOfSections(in collectionView: UICollectionView) -> Int {
 		return 3
 	}
-
+	func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+		switch indexPath.section {
+		case 1:
+			let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "CollectionViewHeader", for: indexPath) as! CollectionViewHeader
+       headerView.viewTitle.text = "Featured Designers"
+      return headerView
+		case 2 :
+			let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "CollectionViewHeader", for: indexPath) as! CollectionViewHeader
+			headerView.viewTitle.text = "Whats Hot"
+			return headerView
+		default:
+			return UICollectionReusableView()
+		}
+	}
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+		switch section {
+		case 1 , 2:
+			return CGSize(width: collectionView.frame.width - 20, height: 35)
+		default:
+			return CGSize(width: 0, height: 0)
+		}
+	}
 	
+	func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+		switch indexPath.section {
+		case 2:
+			let cell = collectionView.cellForItem(at: indexPath) as! BottomCellCollectionViewCell
+			cell.backgroundColor = 	UIColor(white: 0.9, alpha: 1)
+			
+		default:
+			break
+		}
+		
+	}
 	
+	func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+		switch indexPath.section {
+		case 2:
+			let cell = collectionView.cellForItem(at: indexPath) as! BottomCellCollectionViewCell
+			cell.backgroundColor = 	.white
+		default: break
+			
+		}
+	}
 }
 
 
 
-
-
-
-
-
-
-
-
-
+class CollectionViewHeader : UICollectionReusableView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setUpViews()
+	}
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	func setUpViews() {
+		
+		coverView.addSubview(clickButton)
+		coverView.addSubview(viewTitle)
+		addSubview(coverView)
+		coverView.constrainToSuperView(on: self)
+		viewTitle.pintoLeft(superview: coverView,space: 20)
+		viewTitle.centerHorizontalToView(coverView)
+		clickButton.pintoRight(superview: coverView,space: -20)
+		clickButton.centerHorizontalToView(coverView)
+		
+	}
+	lazy var clickButton: UIButton = {
+		let button = UIButton()
+		button.translatesAutoresizingMaskIntoConstraints = false
+		button.setTitle("See all", for: .normal)
+		button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+		button.setTitleColor(#colorLiteral(red: 0.06135459909, green: 0.5319878823, blue: 1, alpha: 1), for: .normal)
+		button.setTitleColor(.lightGray, for: .highlighted)
+		return button
+	}()
+	
+	lazy var coverView: UIView = {
+		let view = UIView()
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.backgroundColor = .white
+		return view
+	}()
+	lazy var viewTitle: UILabel = {
+		let label = UILabel()
+		label.font = UIFont.systemFont(ofSize: 18)
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Section goes here"
+		return label
+	}()
+}
 
 
